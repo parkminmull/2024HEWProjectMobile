edc1870bcc06e8b8a961700db875d20422b9efd9
park sewon <zzong2y@naver.com>
2024-03-04 09:33:28 +0900

    last2

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -9,9 +9,10 @@ const MainPage = () => {
     <div className="h-screen bg-contain b
     g-no-repeat bg-center animate-fadeIn relative">
       <div className="absolute top-0 left-0 right-0 mt-12 text-center z-10">
-        <h1 className="text-4xl font-medium mb-3">Living Hub</h1>
-        <p className="mb-4">プレゼンテーションサイト</p>
-        <p className="text-xl mb-5 animate-rise" >IT42-303<br/>不動産物件情報サイト</p>
+        <h1 className="text-4xl font-medium mb-2">Living Hub</h1>
+        <p className="mb-2">プレゼンテーションサイト</p>
+        <p className='mb-2'>3/05(火) 335教室<br/></p>
+        <p className="text-xl mb-5 animate-rise">IT42-303<br/>不動産物件情報サイト</p>
 
         <Link to="/project-description">
           <button className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded">
